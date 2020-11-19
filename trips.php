6386fa655be6b835d4a35f652ce0a3f5e8879170
TravelTec <59958315+TravelTec@users.noreply.github.com>
2020-11-19 18:01:22 -0300

    Update trips.php

--- trips.php
+++ trips.php
@@ -2,13 +2,18 @@
 /*
 	Plugin Name: WP Viagens
 	Description: This is for updating your Wordpress plugin.
-	Version: 1.5
+	Version: 1.6
 	Author: Travel Tec
-	Author URI: https://traveltec.com.br
+	Author URI: https://traveltec.com
 */
-include_once plugin_dir_path(__FILE__) . '/PDUpdater.php';
+if( ! class_exists( 'Smashing_Updater' ) ){
+	include_once( plugin_dir_path( __FILE__ ) . 'updater.php' );
+}
 
-    $updater = new PDUpdater(__FILE__);
-    $updater->set_username('TravelTec');
-    $updater->set_repository('trips_plugin'); 
-    $updater->initialize();
+$updater = new Smashing_Updater( __FILE__ );
+$updater->set_username( 'TravelTec' );
+$updater->set_repository( 'trips_plugin' );
+/*
+	$updater->authorize( 'abcdefghijk1234567890' ); // Your auth code goes here for private repos
+*/
+$updater->initialize();
