edbfd1eac650dc3c35852c3bc4d9b0eeebdf7c13
Raabe Sampaio <raabe@montenegroev.com.br>
2021-01-18 15:08:11 -0300

    1.0.6 - Atualização tradução
    Atualização da tradução dos step headers da tela de checkout

--- wp-travel-engine.php
+++ wp-travel-engine.php
@@ -4,7 +4,7 @@ Plugin Name: WP Viagens
 Plugin URI: https://github.com/TravelTec/trips_plugin
 GitHub Plugin URI: https://github.com/TravelTec/trips_plugin
 Description: WP Viagens é um plugin de gerenciamento de viagens. Você pode cadastrar seus roteiros, compartilhar promoções, gerenciar suas reservas e manter contato com os clientes que desejam saber mais sobre seus pacotes. Tudo de forma rápida e intuitiva. 
-Version: 1.0.4
+Version: 1.0.6
 Author: Travel Tec
 Author URI: https://traveltec.com.br
 License: GPLv2
